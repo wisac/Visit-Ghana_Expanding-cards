0e79da0cad57dc2dfe3dfe0b6b70e69abecb4ddb
Isaac Wilson <iswilson.dev@gmail.com>
2023-08-20 12:29:37 +0000

    Update: Improve responsiveness

--- main.js
+++ main.js
@@ -20,18 +20,40 @@ function removeActivePanel() {
 
 
 //MOBILE VIEW
-let resizeTimeout;
-
-window.addEventListener('resize', function(event) {
-    clearTimeout(resizeTimeout);
+if (document.body.scrollWidth < 768) {
+    const navigate = document.createElement("div");
+    const prevBtn = document.createElement("button");
+    const nextBtn = document.createElement("button");
+    prevBtn.textContent = "PREV"
+    nextBtn.textContent = "NEXT";
+    document.querySelector("main").appendChild(navigate);
+    navigate.classList.add("navigate-box");
+    navigate.appendChild(prevBtn);
+    navigate.appendChild(nextBtn);
+    nextBtn.classList.add("disabled")
+    nextBtn.classList.add("navigate-btn");
+    prevBtn.classList.add("navigate-btn");
     
-    resizeTimeout = setTimeout(function() {
-        console.log(event)
-        console.log(document.body.scrollHeight);
-    }, 250); // Adjust the delay (in milliseconds) as needed
-});
+
+    
+        
+    
+    console.log("hello");
+}
+// let resizeTimeout;
+
+// window.addEventListener('resize', function(event) {
+//     clearTimeout(resizeTimeout);
+    
+//     resizeTimeout = setTimeout(function() {
+//         console.log(event)
+//         console.log(document.body.scrollHeight);
+//     }, 250); // Adjust the delay (in milliseconds) as needed
+// });
+
+
+
 
 
 // if screenwidth less than mobile range
         //show only one panel and show next and previous buttons
-        
